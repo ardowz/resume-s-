1932841a9b28e10a156d162b46d351657aee3752
Rece <PhilipReasa@gmail.com>
2015-10-18 20:34:27 -0700

    mapped about us out of site

--- basic/config/web.php
+++ basic/config/web.php
@@ -45,6 +45,7 @@ $config = [
             // Disable r= routes
             'enablePrettyUrl' => true,
             'rules' => array(
+                    'about' => 'site/about',
                     '<controller:\w+>/<id:\d+>' => '<controller>/view',
                     '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                     '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
