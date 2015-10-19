76e138920e2410633340fc81365bba8456028cbc
Rece <PhilipReasa@gmail.com>
2015-10-18 21:20:12 -0700

    ditto

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -8,6 +8,7 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
+use Yii\web\view;
 
 AppAsset::register($this);
 ?>
@@ -19,7 +20,8 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <script src='https://code.jquery.com/jquery-2.1.4.min.js'></script>
+    <?php $this->registerJsFile("https://code.jquery.com/jquery-2.1.4.min.js", ['position' => View::POS_HEAD]) ?>
+    <script src=''></script>
     <?php $this->head() ?>
 </head>
 <body>
